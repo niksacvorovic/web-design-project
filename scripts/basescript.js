d9c79f3af68bf2e2cdf7361ec750b1c7e67fd4f6
niksa <cvorovicniksa@gmail.com>
2024-04-08 13:36:17 +0200

    Urađen responsive design na login formi. Dodat X

--- scripts/basescript.js
+++ scripts/basescript.js
@@ -1,6 +1,10 @@
 let popup = document.getElementById("popup");
 let login = document.getElementById("login");
+let close = document.getElementById("close");
 login.addEventListener('click', function(e)
 {
     popup.style.display = "block";
 });
+close.addEventListener('click' , function(e){
+    popup.style.display = "none";
+})
